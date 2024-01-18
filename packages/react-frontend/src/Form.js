6af4e0b32be0bcf30e19f58f999538365fdf4c4f
Christian Bird <chbird@calpoly.edu>
2024-01-18 01:53:18 -0800

    Final commit to React frontend. Added Submission button to form and tested to verify it working

--- packages/react-frontend/src/Form.js
+++ packages/react-frontend/src/Form.js
@@ -1,18 +1,22 @@
 
 import React, { useState } from "react";
 
-function Form() {
+function Form(props) {
     const [person, setPerson] = useState({
       name: "",
       job: ""
     });
+    function submitForm() {
+        props.handleSubmit(person);
+        setPerson({ name: "", job: "" });
+      }
     function handleChange(event) {
         const { name, value } = event.target;
         if (name === "job")
           setPerson({ name: person["name"], job: value });
         else setPerson({ name: value, job: person["job"] });
       }
-    return (
+      return (
         <form>
           <label htmlFor="name">Name</label>
           <input
@@ -30,10 +34,9 @@ function Form() {
             value={person.job}
             onChange={handleChange}
           />
+          <input type="button" value="Submit" onClick={submitForm} />
         </form>
       );
   }
-
- 
 
   export default Form;
